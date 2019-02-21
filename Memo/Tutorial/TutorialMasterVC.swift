76e23a753237f30f1606bc3e3051de48c89a94a4
정기욱 <rldnr56@me.com>
2019-02-21 22:11:48 +0900

    1. UIPageViewControllerDataSource는 페이지 뷰 컨트롤러의 데이터 소스 구현과 관련된 메소도를 구현 할 수 있도록 추가해주는 프로토콜이다
    2. 현재 페이지를 기준으로 앞페이지 뒷페이지를 나타내는 두개의 메소드를 구현한다.
    먼저 인자값으로 받은 뷰 컨트롤러에서 현재의 페이지 인덱스를 추출한뒤 현재의 인덱스가 맨 앞의 값이거나 배열의 데이터 크기보다 크면 nil을 반환하고 그 외의 경우에는 +- 1씩해준다(각각의 메소드에 따라

--- Memo/Tutorial/TutorialMasterVC.swift
+++ Memo/Tutorial/TutorialMasterVC.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class TutorialMasterVC: UIViewController {
+class TutorialMasterVC: UIViewController, UIPageViewControllerDataSource {
+    // UIPageViewControllerDataSource 는 페이지 뷰 컨트롤러의 데이터 소스 구현과 관련된 프로토콜
+    // 여기서 말하는 데이터 소스는, 사용자가 페이지 뷰 컨트롤러의 화면을 앞뒤로 스와이프 할때마다 표시될 콘텐츠 뷰 컨트롤러를 의미함.
+    
     var pageVC: UIPageViewController! // 페이지 뷰 컨트롤러의 인스턴스를 참조할 멤버변수를 선언함.
     
     // 콘텐츠 뷰 컨트롤러의 콘텐츠에 사용될 데이터 배열 선언
@@ -31,5 +34,45 @@ class TutorialMasterVC: UIViewController {
         cvc.imageFile = self.contentImages[idx]
         cvc.pageIndex = idx
         return cvc
+    }
+    
+    // 현재의 콘텐츠 뷰 컨트롤러보다 앞쪽에 올 콘텐츠 뷰 컨트롤러 객체
+    // 즉, 현재의 상태에서 앞쪽으로 스와이프했을 때 보여줄 콘텐츠 뷰 컨트롤러 객체
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        // 현재의 페이지 인덱스 (인자값으로 전달된 뷰 컨트롤러에서 현재의 페이지 인덱스를 추출)
+        guard var index = (viewController as! TutorialContentsVC).pageIndex else {
+            return nil
+        }
+        
+        // 현재의 인덱스가 맨 앞이라면 nil을 반환하고 종료
+        guard index > 0 else {
+            return nil
+        }
+        
+        index -= 1 // 현재의 인덱스에서 하나 뺌(즉, 이전 페이지 인덱스)
+        return self.getContentVC(atIndex: index)
+        
+    }
+    
+    
+    // 현재의 콘텐츠 뷰 컨트롤러보다 뒤쪽에 올 콘텐츠 뷰 컨트롤러 객체
+    // 즉, 현재의 상태에서 뒤쪽으로 스와이프했을 때 보여줄 콘텐츠 뷰 컨트롤러 객체
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        // 현재의 페이지 인덱스
+        guard var index = (viewController as! TutorialContentsVC).pageIndex else {
+            return nil
+        }
+        
+        index += 1
+        
+        // 인덱스는 항상 배열의 데이터 크기보다 작아야한다.
+        guard index < self.contentTitles.count else {
+            return nil
+        }
+        
+        return self.getContentVC(atIndex: index)
     }
 }
