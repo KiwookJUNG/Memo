b4e4312ac8fc17e8dfa1884c2ff950a14eaa9dd0
정기욱 <rldnr56@me.com>
2019-02-21 15:58:15 +0900

    유저의 정보를 관리하는 클래스를 따로 만들어 유저정보를 관리해줌

---
+++ Memo/UserInfoManager.swift
@@ -0,0 +1,105 @@
+//
+//  UserInfoManager.swift
+//  Memo
+//
+//  Created by 정기욱 on 21/02/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+
+struct UserInfoKey {
+    // 저장에 사용할 키
+    static let loginId = "LOGINID"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = "PROFILE"
+}
+
+// 게정 및 사용자 정보를 저장 관리하는 클래스
+
+class UserInfoManager {
+    var loginid: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: UserInfoKey.loginId)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.loginId)
+            ud.synchronize()
+        }
+    }
+    
+    var account: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.account)
+        }
+        set (v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.account)
+            ud.synchronize()
+        }
+    }
+    
+    var name: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.name)
+        }
+        set (v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.name)
+            ud.synchronize()
+        }
+    }
+    
+    var profile: UIImage? {
+        get {
+            let ud = UserDefaults.standard
+            if let _profile = ud.data(forKey: UserInfoKey.profile){
+                return UIImage(data: _profile)
+            } else{
+                return UIImage(named: "account.jpg") // 이미지가 없다면 기본 이미지로
+            }
+        }
+        set (v) {
+            if v != nil {
+                let ud = UserDefaults.standard
+                ud.set(v!.pngData(), forKey: UserInfoKey.profile)
+                ud.synchronize()
+            }
+        }
+    }
+    
+    var isLogin: Bool {
+        // 로그인 아이디가 0 이거나 계정이 비어 있으면
+        if self.loginid == 0 || self.account == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func login(account: String, passwd: String) -> Bool {
+        if account.isEqual("rldnr56@me.com") && passwd.isEqual("1234") {
+            let ud = UserDefaults.standard
+            ud.set(100, forKey: UserInfoKey.loginId)
+            ud.set(account, forKey: UserInfoKey.account)
+            ud.set("정기욱", forKey: UserInfoKey.name)
+            ud.synchronize()
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logout() -> Bool {
+        let ud = UserDefaults.standard
+        ud.removeObject(forKey: UserInfoKey.loginId)
+        ud.removeObject(forKey: UserInfoKey.account)
+        ud.removeObject(forKey: UserInfoKey.name)
+        ud.removeObject(forKey: UserInfoKey.profile)
+        ud.synchronize()
+        return true
+    }
+}
