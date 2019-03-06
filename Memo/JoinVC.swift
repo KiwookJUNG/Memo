4c08ad7c94ce27669ce64e378cbef1174fbb0db4
정기욱 <rldnr56@me.com>
2019-03-06 20:11:07 +0900

    1. 프로필 이미지를 원형으로 설정하고 프로필 이미지에 탭 제스쳐 및 액션 이벤트를 설정해준다.
    2. heightForRowAt 매개변수가 들어간 tableView 메소드는 프로그래밍 방식으로 셀을 만들때 높이를 정해주는 메소드이다.
    
    3. 프로필을 탭 했을 때 액션 메소드는 액션시트창이 뜨도록 해준다.
    각 액션 시트창을 눌렀을때 내부 함수는 UIImagePickerController를 통해 각각에 맞는 source타입을 불러준다.
    
    4. 사용자가 이미지를 선택했다면, 원형 이미지를 UIImage로 타입 캐스팅하여 profile 이미지에 넣어준다.

--- Memo/JoinVC.swift
+++ Memo/JoinVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Alamofire
-class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet var profile: UIImageView!
     
@@ -26,6 +26,14 @@ class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         // 테이블 뷰의 dataSource, delegate 속성 지정
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        // 프로필 이미지를 원형으로 설정
+        self.profile.layer.cornerRadius = self.profile.frame.width / 2
+        self.profile.layer.masksToBounds = true
+        
+        // 프로필 이미지에 탭 제스처 및 액션 이벤트 설정
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tappedProfile(_:)))
+        self.profile.addGestureRecognizer(gesture)
         
     }
     
@@ -65,5 +73,50 @@ class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             ()
         }
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    @objc func tappedProfile(_ sender: Any) {
+        // 전반부) 원하는 소스 타입을 선택할 수 있는 액션 시트 구현
+        let msg = "프로필 이미지를 읽어올 곳을 선택하세요."
+        let sheet = UIAlertController(title: msg, message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "취소", style: .cancel))
+        sheet.addAction(UIAlertAction(title: "저장된 앨범", style: .default, handler: { (_) in
+            selectLibrary(src: .savedPhotosAlbum)
+        }))
+        sheet.addAction(UIAlertAction(title: "포토 라이브러리", style: .default, handler: { (_) in
+            selectLibrary(src: .photoLibrary)
+        }))
+        sheet.addAction(UIAlertAction(title: "카메라", style: .default, handler: { (_) in
+            selectLibrary(src: .camera)
+        }))
+        self.present(sheet, animated: false)
+        
+        // 후반부) 전달된 소스 타입에 맞게 이미지 피커 창을 여는 내부함수
+        func selectLibrary(src: UIImagePickerController.SourceType) {
+            if UIImagePickerController.isSourceTypeAvailable(src) {
+                let picker = UIImagePickerController()
+                picker.delegate = self
+                picker.allowsEditing = true
+                
+                self.present(picker, animated: false)
+            } else {
+                self.alert("사용할 수 없는 타입입니다.")
+            }
+        }
+        
+    }
+    
+    // 사용자가 이미지 피커 컨트롤러에서 이미지를 선택했을 때 실행될 델리게이트 메소드를 작성
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.profile.image = img
+        }
+        self.dismiss(animated: true)
     }
 }
